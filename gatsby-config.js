0515e49bee0782c061cc0f236f47aa05f7e58211
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2018-08-28 12:24:06 -0700

    Added contentful, moved environment access key to .env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+// to separate development & production variables, refer to:
+// https://github.com/gatsbyjs/gatsby/blob/master/docs/docs/environment-variables.md
+
+require("dotenv").config({path: `.env`,})
+
 module.exports = {
   siteMetadata: {
     siteName: `Using Typescript Example`,
@@ -5,5 +10,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },    
   ],
 }
