4d35be19f5c8ec15ab6da160f01c1524b8094b26
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2018-08-28 17:33:20 -0700

    Added link to home on 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
 import Layout from "../layouts"
+import Link from "gatsby-link"
 
 export default () => (
   <Layout>
     <h1>You are here!</h1>
     <h2>But nothing found for you #404</h2>
+    <Link to="/">Go Home</Link>
   </Layout>
 )
