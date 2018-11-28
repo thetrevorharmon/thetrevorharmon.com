0aa6159ad2dc2ba1ac62fa9bdcf6af4408f0cfb1
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2018-11-27 17:44:08 -0800

    Added a bunch of improvements

--- src/templates/caseStudyTemplate.tsx
+++ src/templates/caseStudyTemplate.tsx
@@ -30,26 +30,25 @@ export default class CaseStudyTemplate extends React.Component<CaseStudyTemplate
 
     return (
       <Layout className="case-study-template" >
-        <div className="row post-header">
+        <div className="row post-header my-5">
           <div className="col-lg-6">
-            <Header rank={1} type="Headline">{caseStudy.title}</Header>
-
-            <div className="table-of-contents">
-              <Header rank={2} type="Subtitle">Table of Contents</Header>
+            <Header rank={1} type="Headline" className="mb-0">{caseStudy.title}</Header>
+            <Header rank={2} type="Tagline">{caseStudy.tagline}</Header>
+          </div>
+          <div className="col-lg-6">
+            <div className="hero-header mt-4 mt-lg-0" style={heroStyle} />
+          </div>
+        </div>
+        <div className="row post-body">
+          <div className="col-lg-8 offset-lg-2">
+            <div className="table-of-contents mt-5 mb-7 pl-md-6">
+              <Header rank={3} type="Subtitle">Table of Contents</Header>
               <div
                 dangerouslySetInnerHTML={{
                   __html: caseStudy.tableOfContents.childMarkdownRemark.html,
                 }}
               />
             </div>
-          </div>
-
-          <div className="col-lg-6">
-            <div className="hero-header mt-2" style={heroStyle} />
-          </div>
-        </div>
-        <div className="row post-body">
-          <div className="col-lg-8 offset-lg-2">
             <div
               dangerouslySetInnerHTML={{
                 __html: caseStudy.post.childMarkdownRemark.html,
@@ -70,6 +69,7 @@ export const query = graphql`
           id
           title
           slug
+          tagline
           featureImage {
             resolutions {
               srcSet
