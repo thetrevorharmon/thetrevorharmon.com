01ae9ed4415138edef58fa78d228f19818041271
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-01-29 17:44:51 -0800

    added twitter info

--- src/utils/Helmet.tsx
+++ src/utils/Helmet.tsx
@@ -63,15 +63,19 @@ const Helmet: React.SFC<HelmetDataProps> = ({
       content: `${favicon}`,
       property: 'og:image',
     },
+    {
+      content: 'summary',
+      name: 'twitter:card',
+    },
+    {
+      content: '@thetrevorharmon',
+      name: 'twitter:site',
+    },
+    {
+      content: '@thetrevorharmon',
+      name: 'twitter:creator',
+    },
   ];
-
-// <meta name="twitter:card" content="summary" />
-// <meta name="twitter:site" content="@nytimesbits" />
-// <meta name="twitter:creator" content="@nickbilton" />
-// <meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
-// <meta property="og:title" content="A Twitter for My Sister" />
-// <meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
-// <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
 
   return (
     <ReactHelmet
