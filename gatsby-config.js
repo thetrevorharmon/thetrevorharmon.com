ef48f15751ff60086288b2aa9f5e252feba628bb
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-02-11 17:14:51 -0800

    Fixes for PR

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,14 +48,7 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      // options: {
-      //   rule: {
-      //     include: /assets/
-      //   }
-      // }
-    },  
+    `gatsby-plugin-react-svg`,  
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     `gatsby-plugin-sharp`, // for gatsby-image
