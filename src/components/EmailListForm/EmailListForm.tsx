43c1b04e7b4e6b3fcb8db7a241d2f4d87c892cbb
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-04-20 15:37:37 -0700

    Added starting files for mailchimp signup

---
+++ src/components/EmailListForm/EmailListForm.tsx
@@ -0,0 +1,42 @@
+import addToMailchimp from 'gatsby-plugin-mailchimp';
+
+export default class EmailListForm extends React.Component {
+  // Since `addToMailchimp` returns a promise, you
+  // can handle the response in two different ways:
+
+  // Note that you need to send an email & optionally, listFields
+  // these values can be pulled from React state, form fields,
+  // or wherever.  (Personally, I recommend storing in state).
+
+  // 1. via `.then`
+  _handleSubmit = e => {
+    e.preventDefault();
+    addToMailchimp(email, listFields) // listFields are optional if you are only capturing the email address.
+      .then(data => {
+        // I recommend setting data to React state
+        // but you can do whatever you want (including ignoring this `then()` altogether)
+        console.log(data)
+      })
+      .catch(() => {
+        // unnecessary because Mailchimp only ever
+        // returns a 200 status code
+        // see below for how to handle errors
+      })
+  }
+
+  // 2. via `async/await`
+  _handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await addToMailchimp(email, listFields)
+    // I recommend setting `result` to React state
+    // but you can do whatever you want
+  }
+
+  render() {
+    return (
+      <form onSubmit={this._handleSubmit(email, { listFields })}>
+        ...
+      </form>
+    )
+  }
+}
