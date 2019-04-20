7e496eb88df34bc99cc2da7127254bd67bd0b01a
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-04-20 15:47:55 -0700

    added typings for list form

--- src/components/EmailListForm/EmailListForm.tsx
+++ src/components/EmailListForm/EmailListForm.tsx
@@ -1,4 +1,5 @@
 import addToMailchimp from 'gatsby-plugin-mailchimp';
+import * as React from 'react';
 
 export default class EmailListForm extends React.Component {
   // Since `addToMailchimp` returns a promise, you
@@ -9,33 +10,33 @@ export default class EmailListForm extends React.Component {
   // or wherever.  (Personally, I recommend storing in state).
 
   // 1. via `.then`
-  _handleSubmit = e => {
-    e.preventDefault();
-    addToMailchimp(email, listFields) // listFields are optional if you are only capturing the email address.
-      .then(data => {
-        // I recommend setting data to React state
-        // but you can do whatever you want (including ignoring this `then()` altogether)
-        console.log(data)
-      })
-      .catch(() => {
-        // unnecessary because Mailchimp only ever
-        // returns a 200 status code
-        // see below for how to handle errors
-      })
-  }
+  // _handleSubmit = e => {
+  //   e.preventDefault();
+  //   addToMailchimp(email, listFields) // listFields are optional if you are only capturing the email address.
+  //     .then(data => {
+  //       // I recommend setting data to React state
+  //       // but you can do whatever you want (including ignoring this `then()` altogether)
+  //       console.log(data)
+  //     })
+  //     .catch(() => {
+  //       // unnecessary because Mailchimp only ever
+  //       // returns a 200 status code
+  //       // see below for how to handle errors
+  //     })
+  // }
 
   // 2. via `async/await`
-  _handleSubmit = async (e) => {
-    e.preventDefault();
-    const result = await addToMailchimp(email, listFields)
-    // I recommend setting `result` to React state
-    // but you can do whatever you want
-  }
+  // _handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const result = await addToMailchimp(email, listFields)
+  //   // I recommend setting `result` to React state
+  //   // but you can do whatever you want
+  // }
 
-  render() {
+  public render() {
     return (
-      <form onSubmit={this._handleSubmit(email, { listFields })}>
-        ...
+      <form>
+        <button type="submit">Submit!</button>
       </form>
     )
   }
