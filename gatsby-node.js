a8d0462f93d2e08179444457f6ad0a318f169a93
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-07-18 15:45:17 -0700

    Generating pages correctly

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,48 @@
  */
 const path = require(`path`)
 
+// This combines blog posts and link posts into a single array of posts
+// First combines and then sorts according to date (newest first)
+const combinePostTypes = (blogPosts, linkPosts, order = 'desc') => {
+  const orderMultiplier = order === 'desc' ? 1 : -1;
+
+  const posts = [
+    // blog posts
+    ...blogPosts.edges.map((edge) => edge.node),
+    // link posts
+    ...linkPosts.edges.map((edge) => edge.node),
+  ].sort(
+    (firstDate, secondDate) => {
+      const a = new Date(firstDate.date);
+      const b = new Date(secondDate.date);
+
+      if (a < b) { return 1 * orderMultiplier; }
+      if (a > b) { return -1 * orderMultiplier; }
+
+      return 0;
+    },
+  );
+
+  return posts;
+}
+
+// Given a type, this returns the location of the template for the type
+// It throws an error when it encounters a type that doesn't exist
+const pathTemplateForPostType = (type) => {
+  const paths = {
+    ContentfulBlogPost: `./src/templates/post/blog/blogPostTemplate.tsx`,
+    ContentfulLinkPost: `./src/templates/post/blog/linkPostTemplate.tsx`,
+    ContentfulProject: `./src/templates/project/projectTemplate.tsx`,
+    ContentfulCaseStudy: `./src/templates/post/caseStudy/caseStudyTemplate.tsx`,
+  }
+
+  if (!paths[type]) {
+    throw new Error(`Cannot find template path for type: ${type}`);
+  }
+
+  return paths[type];
+}
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return new Promise((resolve, reject) => {
@@ -15,6 +57,9 @@ exports.createPages = ({ graphql, actions }) => {
             node {
               title
               slug
+              internal {
+                type
+              }              
             }
           }
         }
@@ -23,6 +68,9 @@ exports.createPages = ({ graphql, actions }) => {
             node {
               title
               slug
+              internal {
+                type
+              }              
             }
           }
         } 
@@ -31,16 +79,30 @@ exports.createPages = ({ graphql, actions }) => {
             node {
               title
               slug
+              internal {
+                type
+              }     
             }
           }
-        }   
+        }
+        allContentfulLinkPost(sort: { order: DESC, fields: [date] })  {
+          edges {
+            node {
+              title
+              slug
+              internal {
+                type
+              }
+            }
+          }
+        }           
       }
     `
   ).then(result => {
       result.data.allContentfulProject.edges.forEach(({ node }) => {
         createPage({
           path: `projects/${node.slug}`,
-          component: path.resolve(`./src/templates/project/projectTemplate.tsx`),
+          component: path.resolve(pathTemplateForPostType(node.internal.type)),
           context: {
             slug: node.slug,
           },
@@ -49,33 +111,35 @@ exports.createPages = ({ graphql, actions }) => {
       result.data.allContentfulCaseStudy.edges.forEach(({ node }) => {
         createPage({
           path: `case-studies/${node.slug}`,
-          component: path.resolve(`./src/templates/post/caseStudy/caseStudyTemplate.tsx`),
+          component: path.resolve(pathTemplateForPostType(node.internal.type)),
           context: {
             slug: node.slug,
           },
         })
-      })  
-      result.data.allContentfulBlogPost.edges.forEach(({ node }, index) => {
+      });
+
+      const posts = combinePostTypes(
+        result.data.allContentfulBlogPost,
+        result.data.allContentfulLinkPost,
+      )
+      
+      posts.forEach((node, index) => {
         // these give an easy way to figure out which post is considered
         // the next newer/older post from within a blog post
-        const newerPost = index > 0 
-          ? result.data.allContentfulBlogPost.edges[index - 1].node 
-          : null;
-
-        const olderPost = index < result.data.allContentfulBlogPost.edges.length - 1 
-          ? result.data.allContentfulBlogPost.edges[index + 1].node 
-          : null;
+        const newerPost = index > 0 ? posts[index - 1].node : null;
+        const olderPost = index < posts.length - 1 ? posts[index + 1].node : null;
 
         createPage({
           path: `blog/${node.slug}`,
-          component: path.resolve(`./src/templates/post/blog/blogPostTemplate.tsx`),
+          component: path.resolve(pathTemplateForPostType(node.internal.type)),
           context: {
             slug: node.slug,
             newerPost: newerPost,
             olderPost: olderPost,
           },
         })
-      })            
+      });
+
       resolve()
     })
   })
--- src/templates/post/blog/linkPostTemplate.tsx
+++ src/templates/post/blog/linkPostTemplate.tsx
@@ -2,7 +2,7 @@ import { graphql } from 'gatsby';
 import * as React from 'react';
 import { Layout } from '../../../layouts';
 
-import './blogPostTemplate.scss';
+import './LinkPostTemplate.scss';
 
 import {
   Header,
@@ -19,12 +19,12 @@ import { Routes } from '../../../utils';
 
 type PostNavigationDirection = 'older' | 'newer';
 
-interface BlogPostTemplateProps {
+interface LinkPostTemplateProps {
   data: {
-    allContentfulBlogPost: {
+    allContentfulLinkPost: {
       edges: [
         {
-          node: BlogPost,
+          node: LinkPost,
         },
       ],
     },
@@ -45,14 +45,14 @@ interface BlogPostTemplateProps {
   };
 }
 
-export default class BlogPostTemplate extends React.Component<BlogPostTemplateProps, {}> {
+export default class LinkPostTemplate extends React.Component<LinkPostTemplateProps, {}> {
 
   public makeNavigation = (title: string, slug: string, direction: PostNavigationDirection) => {
     const className = `post-link ${direction}-post`;
 
     return (
       <div className="col-lg-6 post-navigation">
-        Read {direction}:<br/>
+        Read {direction}:<br />
         <Link href={Routes.blogPost(slug)} className={className}>
           <span className="title">{title}</span>
         </Link>
@@ -60,7 +60,7 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
     );
   }
 
-  public twitterShareUrl = (siteData: SiteMetadata, blogPost: BlogPost) => {
+  public twitterShareUrl = (siteData: SiteMetadata, blogPost: (BlogPost | LinkPost)) => {
     const twitterText = encodeURI(`I just finished reading "${blogPost.title}" by ${siteData.twitter.author}`);
     const postAbsoluteUrl = `${siteData.siteUrl}${Routes.blogPost(blogPost.slug)}`;
     const shareUrl = `https://twitter.com/intent/tweet?url=${postAbsoluteUrl}&text=${twitterText}`;
@@ -70,7 +70,7 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
 
   public render() {
 
-    const blogPost = this.props.data.allContentfulBlogPost.edges[0].node;
+    const linkPost = this.props.data.allContentfulLinkPost.edges[0].node;
     const siteData = this.props.data.site.siteMetadata;
 
     const {
@@ -80,9 +80,9 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
     } = this.props.pageContext;
 
     const pageMetadata: PageMetadata = {
-      description: `${blogPost.description}`,
-      image: blogPost.heroImage && blogPost.heroImage.fluid.src,
-      title: `${blogPost.title}`,
+      description: `${linkPost.description}`,
+      // image: blogPost.heroImage && blogPost.heroImage.fluid.src,
+      title: `${linkPost.title}`,
       url: Routes.blogPost(slug),
     };
 
@@ -90,21 +90,21 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
       const className = `${attribution.type.toLowerCase()}-attribution`;
 
       return attribution.type === 'Photo'
-      ? (
-        <div className={className}>
-          {`${attribution.type} by ${attribution.author} on `}
-          <Link href={attribution.sourceLocation}>
-            {attribution.sourceName}
-          </Link>.
+        ? (
+          <div className={className}>
+            {`${attribution.type} by ${attribution.author} on `}
+            <Link href={attribution.sourceLocation}>
+              {attribution.sourceName}
+            </Link>.
         </div>
-      ) : (
-        <div className={className}>
-          This {`${attribution.type.toLowerCase()} was originally published on `}
-          <Link href={attribution.sourceLocation}>
-            {attribution.sourceName}
-          </Link>.
+        ) : (
+          <div className={className}>
+            This {`${attribution.type.toLowerCase()} was originally published on `}
+            <Link href={attribution.sourceLocation}>
+              {attribution.sourceName}
+            </Link>.
         </div>
-      );
+        );
     };
 
     const pageLayoutClassName = 'col-lg-7';
@@ -112,21 +112,21 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
     return (
       <Layout className="blog-post-template" pageMetadata={pageMetadata}>
         <div className="row post-header mt-4 mt-lg-6 mb-2 mb-lg-4">
-          {
+          {/* {
             blogPost.heroImage && (
               <div className={pageLayoutClassName}>
                 <Image src={blogPost.heroImage} />
                 {blogPost.photoAttribution && makeAttribution(blogPost.photoAttribution)}
               </div>
             )
-          }
+          } */}
 
           <div className={pageLayoutClassName}>
-            <Header rank={1} type="Headline" className="mb-0">{blogPost.title}</Header>
-            {blogPost.subtitle && (
-              <Header rank={2} type="Tagline" className="mt-1">{blogPost.subtitle}</Header>
+            <Header rank={1} type="Headline" className="mb-0">{linkPost.title}</Header>
+            {linkPost.subtitle && (
+              <Header rank={2} type="Tagline" className="mt-1">{linkPost.subtitle}</Header>
             )}
-            <p className="meta">{blogPost.date} • {blogPost.body.childMarkdownRemark.timeToRead} min read</p>
+            <p className="meta">{linkPost.date} • {linkPost.body.childMarkdownRemark.timeToRead} min read</p>
           </div>
         </div>
         <div className="post-body">
@@ -135,13 +135,13 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
             <div className={pageLayoutClassName}>
               <div
                 dangerouslySetInnerHTML={{
-                  __html: blogPost.body.childMarkdownRemark.html,
+                  __html: linkPost.body.childMarkdownRemark.html,
                 }}
               />
             </div>
           </div>
 
-          {
+          {/* {
             blogPost.sourceAttribution && (
               <div className="row">
                 <div className={pageLayoutClassName}>
@@ -149,7 +149,7 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
                 </div>
               </div>
             )
-          }
+          } */}
         </div>
         <div className="row my-5 my-lg-6">
           <div className={pageLayoutClassName}>
@@ -157,7 +157,7 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
               <Link href={siteData.feedUrl} isIconLink={true} className="icon-link">
                 <Icon name="rss" />
               </Link>
-              <Link href={this.twitterShareUrl(siteData, blogPost)} isIconLink={true} className="ml-2 icon-link">
+              <Link href={this.twitterShareUrl(siteData, linkPost)} isIconLink={true} className="ml-2 icon-link">
                 <Icon name="twitter" />
               </Link>
             </EmailListForm>
@@ -176,29 +176,18 @@ export default class BlogPostTemplate extends React.Component<BlogPostTemplatePr
 
 export const query = graphql`
   query($slug: String!) {
-    allContentfulBlogPost(filter: {slug: {eq: $slug}}) {
+    allContentfulLinkPost(filter: {slug: {eq: $slug}}) {
       edges {
         node {
-          heroImage {
-            ...ContentfulAsset_width750
-          }
           title
           slug
-          subtitle
-          description
+          link
           date(formatString: "MMMM DD, YYYY")
           body {
             childMarkdownRemark {
               html
               timeToRead
             }
-          }
-          tags
-          sourceAttribution {
-            ...ContentfulAttribution
-          }
-          photoAttribution {
-            ...ContentfulAttribution
           }
         }
       }
