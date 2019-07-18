bec6253bad3711bc7fa2787f9752d227024e42b0
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-07-18 12:53:55 -0700

    added explicit undefined to host for contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: environment === 'development' && `preview.contentful.com`,
+        host: environment === 'development' ? `preview.contentful.com` : undefined,
       },
     },
     `gatsby-plugin-typescript`,
