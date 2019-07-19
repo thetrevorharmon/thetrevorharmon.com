85eeaf272655307250b2b986456a178b3e3cc64b
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-07-19 15:41:39 -0700

    added link to body of linkpost

--- src/templates/post/blog/components/PostBody/PostBody.tsx
+++ src/templates/post/blog/components/PostBody/PostBody.tsx
@@ -2,6 +2,7 @@ import classnames from 'classnames';
 import * as React from 'react';
 
 import {
+  Button,
   Link,
 } from '../../../../../UI-Kit';
 
@@ -48,6 +49,15 @@ const PostBody: React.FC<PostBodyProps> = (props: PostBodyProps) => {
           <div className="row">
             <div className={layoutClassName}>
               {makeSourceAttribution(post.sourceAttribution)}
+            </div>
+          </div>
+        )
+      }
+      {
+        post.postType === 'Link' && post.link && (
+          <div className="row mb-lg-9">
+            <div className={layoutClassName}>
+              <Button href={post.link}>View Link</Button>
             </div>
           </div>
         )
