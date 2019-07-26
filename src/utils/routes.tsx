a0e2143cd83f615eaa50525b5f5c780e710a3b47
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2019-07-26 15:37:49 -0700

    Fixed github url

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -3,7 +3,7 @@ interface LinkInterface {
 }
 
 const ExternalLinks: LinkInterface = {
-  github: () => 'http://github.com/tdharmon',
+  github: () => 'http://github.com/thetrevorharmon',
   linkedIn: () => 'http://linkedin.com/in/trevorharmon',
   medium: () => 'http://medium.com/@thetrevorharmon',
   mediumPost: (slug: string = '') => `http://medium.com/@thetrevorharmon/${slug}`,
