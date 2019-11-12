a48e27c094d4e40f6f03087c7c03ad0cb80d8c1a
Trevor Harmon <trevor.harmon@shopify.com>
2019-11-11 16:27:57 -0800

    reverted header.tsx

--- src/UI-Kit/Header/Header.tsx
+++ src/UI-Kit/Header/Header.tsx
@@ -1,28 +1,34 @@
 import classnames from 'classnames';
-import React, {useContext} from 'react';
+import * as React from 'react';
+import Link from '../Link';
+
 import * as styles from './Header.module.scss';
-// import {Theme} from '../../utils/Theme';
-import {useTheme} from '../../context/ThemeContext';
 
 export type HeaderRank = 0 | 1 | 2 | 3 | 4 | 5 | 6;
-export type HeaderType = 'Display' | 'Title' | 'Heading' | 'Subheading';
+export type HeaderType =
+  | 'Headline'
+  | 'Tagline'
+  | 'Title'
+  | 'Subtitle'
+  | 'SectionTitle';
 
 interface HeaderProps {
   rank: HeaderRank;
   type: HeaderType;
   className?: string;
-  children: React.ReactNode;
 }
 
-const Header = ({rank, type, className, children}: HeaderProps) => {
-  const theme = useTheme();
-  const classname = classnames(styles[`Header-${theme}`], className);
-
-  console.log(`Header-${theme}`);
+const Header: React.FC<HeaderProps> = ({rank, type, className, children}) => {
+  const classname = classnames(styles.Header, styles[`${type}`], className);
 
   const Tag = rank < 1 || rank > 6 ? 'span' : `h${rank}`;
 
   return <Tag className={classname}>{children}</Tag>;
+};
+
+Header.defaultProps = {
+  rank: 2,
+  type: 'Title',
 };
 
 export default Header;
