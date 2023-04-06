23e151b9fef0ebee0536c19da6b6e14e4e2b2290
Trevor Harmon <trevor.harmon@shopify.com>
2023-04-06 15:05:50 -0400

    Added token table

--- src/content/blog/drafts/connecting-antlr-to-codemirror-6-building-a-language-server/CodeEditorWithTokens/CodeEditorWithTokens.tsx
+++ src/content/blog/drafts/connecting-antlr-to-codemirror-6-building-a-language-server/CodeEditorWithTokens/CodeEditorWithTokens.tsx
@@ -1,6 +1,7 @@
 import React, {useMemo, useState} from 'react';
 import {CodeEditor} from '../../../../../components/CodeEditor';
 import {Zephyr} from '../../../../../components/CodeEditor/language';
+import {Breakout} from '../../../../../UI-Kit';
 
 interface Props {
   initialValue?: string;
@@ -11,12 +12,51 @@ export function CodeEditorWithTokens({initialValue}: Props) {
 
   const languageServer = useMemo(() => new Zephyr(), []);
   const tokens = languageServer.getTokenStream(value).map((token) => ({
-    index: token.tokenIndex,
-    range: [token.startIndex, token.stopIndex],
+    text:
+      token.type === -1
+        ? '<EOF>'
+        : value.slice(token.startIndex, token.stopIndex + 1),
     type: languageServer.getTokenTypeForIndex(token.type),
+    index: token.tokenIndex,
+    range: `[${token.startIndex}, ${token.stopIndex}]`,
   }));
 
-  console.log(tokens);
+  const cellClasses = 'text-left px-small py-tiny';
 
-  return <CodeEditor initialValue={value} updateValue={setValue} />;
+  const headers = Object.keys(tokens[0]);
+  const tableHeaderMarkup = (
+    <tr>
+      {headers.map((header) => (
+        <th
+          className={`${cellClasses} bg-[#dedede] dark:bg-[#2f2f2f] border-b border-[#ddd] dark:border-[#555]`}
+        >
+          {header}
+        </th>
+      ))}
+    </tr>
+  );
+
+  const tableBodyMarkup = tokens.map((token) => {
+    const values = Object.values(token);
+
+    return (
+      <tr className="even:bg-[#e2e2e2] even:dark:bg-[#202020]">
+        {values.map((value) => (
+          <td className={`${cellClasses}`}>{value}</td>
+        ))}
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <CodeEditor initialValue={value} updateValue={setValue} />
+      <Breakout className="bg-caption-bg dark:bg-caption-bg-dark rounded-md overflow-hidden border border-[#ddd] dark:border-[#555] text-[0.95rem] max-h-[300px] overflow-y-auto">
+        <table className="w-full font-mono ">
+          <thead>{tableHeaderMarkup}</thead>
+          <tbody>{tableBodyMarkup}</tbody>
+        </table>
+      </Breakout>
+    </>
+  );
 }
