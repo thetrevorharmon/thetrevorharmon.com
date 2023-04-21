5115b959aca4e5dc29c8da90dc4e605969a143a9
Trevor Harmon <9555730+thetrevorharmon@users.noreply.github.com>
2023-04-21 12:36:56 -0400

    🔧 `create-blog-post` script (#155)

---
+++ scripts/create-blog-post.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const slugify = require('slugify');
+const {program} = require('commander');
+
+program
+  .option('--title <title>', 'Title of the blog post')
+  .action(({title}) => {
+    if (!title) {
+      console.error('Please provide a title for the blog post');
+      process.exit(1);
+    }
+
+    const slug = slugify(title, {lower: true});
+
+    const folderPath = path.join(
+      __dirname,
+      '..',
+      'src',
+      'content',
+      'blog',
+      'drafts',
+      slug,
+    );
+
+    const filePath = path.join(folderPath, `${slug}.mdx`);
+
+    if (fs.existsSync(folderPath)) {
+      console.error(`Folder ${slug} already exists`);
+      process.exit(1);
+    }
+
+    fs.mkdirSync(folderPath);
+
+    const today = new Date().toISOString().slice(0, 10);
+
+    const frontmatter = `---
+title: "${title}"
+date: ${today}
+slug: "${slug}"
+type: Post
+status: Draft
+description: ""
+---
+
+Content goes here
+`;
+
+    fs.writeFileSync(filePath, frontmatter);
+
+    console.log(`Blog post ${title} created at ${folderPath}`);
+  })
+  .parse(process.argv);
