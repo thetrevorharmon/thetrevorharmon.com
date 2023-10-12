d94bb2d5b61eb230e3b3098c1885299983d059a6
Trevor Harmon <thetrevorharmon@gmail.com>
2023-10-12 11:02:11 -0400

    🛠️ Add Link as a shortcode in MDX documents

--- src/components/MdxProvider/MdxProvider.tsx
+++ src/components/MdxProvider/MdxProvider.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {MDXProvider} from '@mdx-js/react';
 import {Callout} from '../Callout';
 import {TwoUp} from '../TwoUp';
+import {Link} from '../Link';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const shortcodes = {Callout, TwoUp};
+const shortcodes = {Callout, TwoUp, Link};
 
 export function MdxProvider({children}: Props) {
   return <MDXProvider components={shortcodes}>{children}</MDXProvider>;
