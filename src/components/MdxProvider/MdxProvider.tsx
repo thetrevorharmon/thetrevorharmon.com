71deeb25e1b454fd66965b2b3dc716a967f002f4
Trevor Harmon <thetrevorharmon@gmail.com>
2024-07-30 13:44:29 +0200

    ✨ Move Citation to shortcodes

--- src/components/MdxProvider/MdxProvider.tsx
+++ src/components/MdxProvider/MdxProvider.tsx
@@ -3,12 +3,13 @@ import {MDXProvider} from '@mdx-js/react';
 import {Callout} from '../Callout';
 import {TwoUp} from '../TwoUp';
 import {Link} from '../Link';
+import {Citation} from '../Citation';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const shortcodes = {Callout, TwoUp, Link};
+const shortcodes = {Callout, TwoUp, Link, Citation};
 
 export function MdxProvider({children}: Props) {
   return <MDXProvider components={shortcodes}>{children}</MDXProvider>;
